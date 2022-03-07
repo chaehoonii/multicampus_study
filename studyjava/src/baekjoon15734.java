97f115175d2ab2abf9d7b7aaa33c164dee66a0f2
chaehooni <46rlacogns@gmail.com>
2022-03-07 20:57:31 +0900

    baekjoon2576
    2022-03-07

---
+++ studyjava/src/baekjoon15734.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class baekjoon15734 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int R = sc.nextInt();
+		int L = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if(R==L) {
+			System.out.println(R+L+B/2*2);
+		}
+		else if(R>L) {
+			//ÃÖ¼Ò°ª*2 
+		}
+
+	}
+
+}
